aa7d48ba4d42cf43e2ea5073baa3fca71955b1c6
Antonio <antoniorodrigo92@gmail.com>
2026-03-16 11:58:14 +0000

    adicionar CAST

--- src/main/scala/transformations/ArrayColumnToColumns.scala
+++ src/main/scala/transformations/ArrayColumnToColumns.scala
@@ -1,7 +1,8 @@
 package transformations
 
-import Utils.Constants.{AWAY_TEAM, AWAY_TEAM_LST_AWAY_MATCH_RESULT_LAST_2_GAMES, AWAY_TEAM_LST_MATCH_RESULT_LAST_5_GAMES, FINAL_TABLE_COLUMNS, HOME_TEAM, HOME_TEAM_LST_HOME_MATCH_RESULT_LAST_2_GAMES, HOME_TEAM_LST_MATCH_RESULT_LAST_5_GAMES}
+import Utils.Constants.{AWAY_TEAM, AWAY_TEAM_LST_AWAY_MATCH_RESULT_LAST_2_GAMES, AWAY_TEAM_LST_MATCH_RESULT_LAST_5_GAMES, FINAL_TABLE_COLUMNS, GAME_DATE, GAME_DATETIME, GAME_TIME, HOME_TEAM, HOME_TEAM_LST_HOME_MATCH_RESULT_LAST_2_GAMES, HOME_TEAM_LST_MATCH_RESULT_LAST_5_GAMES}
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.col
 import transformations.CommonFunctions.listToColumns
 
 object ArrayColumnToColumns {
@@ -13,6 +14,9 @@ object ArrayColumnToColumns {
       .transform(listToColumns(AWAY_TEAM_LST_MATCH_RESULT_LAST_5_GAMES, 5, AWAY_TEAM))
       .transform(listToColumns(AWAY_TEAM_LST_AWAY_MATCH_RESULT_LAST_2_GAMES, 2, f"${AWAY_TEAM}_away"))
       .select(FINAL_TABLE_COLUMNS.head, FINAL_TABLE_COLUMNS.tail: _*)
+      .withColumn(GAME_DATETIME, col(GAME_DATETIME).cast("timestamp"))
+      .withColumn(GAME_DATE, col(GAME_DATE).cast("timestamp"))
+      .withColumn(GAME_TIME, col(GAME_TIME).cast("timestamp"))
   }
 
 }
