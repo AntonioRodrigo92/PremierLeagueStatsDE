be0200859a1b86bcfbfffc4646a7ba5c51ab6ac0
Antonio <antoniorodrigo92@gmail.com>
2026-03-17 12:27:15 +0000

    adicionar CAST

--- src/main/scala/transformations/ArrayColumnToColumns.scala
+++ src/main/scala/transformations/ArrayColumnToColumns.scala
@@ -3,6 +3,7 @@ package transformations
 import Utils.Constants.{AWAY_TEAM, AWAY_TEAM_LST_AWAY_MATCH_RESULT_LAST_2_GAMES, AWAY_TEAM_LST_MATCH_RESULT_LAST_5_GAMES, FINAL_TABLE_COLUMNS, GAME_DATE, GAME_DATETIME, GAME_TIME, HOME_TEAM, HOME_TEAM_LST_HOME_MATCH_RESULT_LAST_2_GAMES, HOME_TEAM_LST_MATCH_RESULT_LAST_5_GAMES}
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.types.{DateType, StringType, TimestampType}
 import transformations.CommonFunctions.listToColumns
 
 object ArrayColumnToColumns {
@@ -14,9 +15,9 @@ object ArrayColumnToColumns {
       .transform(listToColumns(AWAY_TEAM_LST_MATCH_RESULT_LAST_5_GAMES, 5, AWAY_TEAM))
       .transform(listToColumns(AWAY_TEAM_LST_AWAY_MATCH_RESULT_LAST_2_GAMES, 2, f"${AWAY_TEAM}_away"))
       .select(FINAL_TABLE_COLUMNS.head, FINAL_TABLE_COLUMNS.tail: _*)
-      .withColumn(GAME_DATETIME, col(GAME_DATETIME).cast("timestamp"))
-      .withColumn(GAME_DATE, col(GAME_DATE).cast("timestamp"))
-      .withColumn(GAME_TIME, col(GAME_TIME).cast("timestamp"))
+      .withColumn(GAME_DATETIME, col(GAME_DATETIME).cast(TimestampType))
+      .withColumn(GAME_DATE, col(GAME_DATE).cast(DateType))
+      .withColumn(GAME_TIME, col(GAME_TIME).cast(StringType))
   }
 
 }
