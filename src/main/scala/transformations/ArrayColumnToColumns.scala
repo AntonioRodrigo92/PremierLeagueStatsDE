65fb10ca5613dd6e64d9ab5bfef469266d6be89a
Antonio <antoniorodrigo92@gmail.com>
2026-03-17 16:46:36 +0000

    adicionar CAST

--- src/main/scala/transformations/ArrayColumnToColumns.scala
+++ src/main/scala/transformations/ArrayColumnToColumns.scala
@@ -2,7 +2,7 @@ package transformations
 
 import Utils.Constants.{AWAY_TEAM, AWAY_TEAM_LST_AWAY_MATCH_RESULT_LAST_2_GAMES, AWAY_TEAM_LST_MATCH_RESULT_LAST_5_GAMES, FINAL_TABLE_COLUMNS, GAME_DATE, GAME_DATETIME, GAME_TIME, HOME_TEAM, HOME_TEAM_LST_HOME_MATCH_RESULT_LAST_2_GAMES, HOME_TEAM_LST_MATCH_RESULT_LAST_5_GAMES}
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions.{col, substring}
 import org.apache.spark.sql.types.{DateType, StringType, TimestampType}
 import transformations.CommonFunctions.listToColumns
 
@@ -17,7 +17,7 @@ object ArrayColumnToColumns {
       .select(FINAL_TABLE_COLUMNS.head, FINAL_TABLE_COLUMNS.tail: _*)
       .withColumn(GAME_DATETIME, col(GAME_DATETIME).cast(TimestampType))
       .withColumn(GAME_DATE, col(GAME_DATE).cast(DateType))
-      .withColumn(GAME_TIME, col(GAME_TIME).cast(StringType))
+      .withColumn(GAME_TIME, substring(col(GAME_TIME).cast(StringType), -8, 8))
   }
 
 }
